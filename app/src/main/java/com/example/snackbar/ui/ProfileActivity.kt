aa78581fb481a3206edb697a19a20e14d68de2e6
Marina <marina.ssaito@gmail.com>
2020-10-28 21:35:42 -0300

    Até o passo 6

--- app/src/main/java/com/example/snackbar/ui/ProfileActivity.kt
+++ app/src/main/java/com/example/snackbar/ui/ProfileActivity.kt
@@ -2,12 +2,32 @@ package com.example.snackbar.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.snackbar.R
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.view.*
+
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var navController : NavController
+    private lateinit var appBarConfiguration : AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        setSupportActionBar(profileToolbar)
+        navController = findNavController(R.id.action_profileFragment_to_ajudaFragment)
+        //appBarConfiguration = AppBarConfiguration(navController.graph)
+        //setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
---
+++ app/src/main/java/com/example/snackbar/ui/ProfileFragment.kt
@@ -0,0 +1,33 @@
+package com.example.snackbar.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.snackbar.R
+import kotlinx.android.synthetic.main.fragment_profile.view.*
+
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view : View =  inflater.inflate(R.layout.fragment_profile, container, false)
+
+        view.btn_ajuda.setOnClickListener{
+            findNavController().navigate(R.id.action_profileFragment_to_ajudaFragment)
+        }
+
+        return view
+    }
+
+    companion object{
+        fun newInstance() = ProfileFragment()
+    }
+}
